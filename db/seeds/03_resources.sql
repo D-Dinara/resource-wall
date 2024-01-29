51566372fe7bfb4163eb0b733a52dbf008eb1940
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-01-29 11:25:22 -0500

    create seeds for CSS category resources

--- db/seeds/03_resources.sql
+++ db/seeds/03_resources.sql
@@ -1,17 +1,31 @@
 -- Javascript Category
 INSERT INTO resources(creator_id, category_id, url, title, description, rating, thumbnail_url)
 -- should have no image
-VALUES(4, 1, "https://auth.udacity.com/sign-up?next=https%3A%2F%2Flearn.udacity.com%2Fcourses%2Fud803%2Flessons%2Ff2c28aac-882f-4853-8f65-12bbc7aa3a52%2Fconcepts%2F477447d2-adb8-42f1-9620-1f63af838fe0", "Udacity: Data Types & Variables", "Interactive coding challenges for all levels, with instant feedback and progress tracking.", 3),
-VALUES(7, 1, "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration", "MDN: Loops and Iteration", "Comprehensive coding tutorials covering languages, frameworks, and tools, with practical examples and exercises.", 5, '../../assets/resource-thumbs/javascript.jpg'),
-VALUES(2, 1, "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions", "MDN: Functions", "Online coding bootcamp offering immersive, mentor-led programs in web development, data science, and more.", 2, '../../assets/resource-thumbs/books.jpg'),
+VALUES(4, 1, 'https://auth.udacity.com/sign-up?next=https%3A%2F%2Flearn.udacity.com%2Fcourses%2Fud803%2Flessons%2Ff2c28aac-882f-4853-8f65-12bbc7aa3a52%2Fconcepts%2F477447d2-adb8-42f1-9620-1f63af838fe0', 'Udacity: Data Types & Variables', 'Interactive coding challenges for all levels, with instant feedback and progress tracking.', 3),
+VALUES(7, 1, 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration', 'MDN: Loops and Iteration', 'Comprehensive coding tutorials covering languages, frameworks, and tools, with practical examples and exercises.', 5, '../../assets/resource-thumbs/javascript.jpg'),
+VALUES(2, 1, 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions', 'MDN: Functions', 'Online coding bootcamp offering immersive, mentor-led programs in web development, data science, and more.', 2, '../../assets/resource-thumbs/books.jpg'),
 -- should have no image
-VALUES(6, 1, "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array", "MDN: Arrays", "Platform providing coding courses, projects, and assessments, with a focus on real-world applications and industry relevance.", 2),
-VALUES(3, 1, "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects", "MDN: Objects", "Community-driven platform for coding enthusiasts, featuring tutorials, forums, and collaborative coding projects.", 4, '../../assets/resource-thumbs/working-hard.jpg'),
-VALUES(8, 1, "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator", "Ternary Operators", "Video-based coding tutorials covering a wide range of topics, from beginner to advanced levels.", 1, '../../assets/resource-thumbs/design.jpg'),
+VALUES(6, 1, 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array', 'MDN: Arrays', 'Platform providing coding courses, projects, and assessments, with a focus on real-world applications and industry relevance.', 2),
+VALUES(3, 1, 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects', 'MDN: Objects', 'Community-driven platform for coding enthusiasts, featuring tutorials, forums, and collaborative coding projects.', 4, '../../assets/resource-thumbs/working-hard.jpg'),
+VALUES(8, 1, 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator', 'Ternary Operators', 'Video-based coding tutorials covering a wide range of topics, from beginner to advanced levels.', 1, '../../assets/resource-thumbs/design.jpg'),
 -- should have no image
-VALUES(1, 1, "https://dev.to/devsmitra/28-javascript-array-hacks-a-cheat-sheet-for-developer-5769", "Array Hacks Cheat Sheet", "Personalized coding learning paths tailored to individual goals and skill levels, with adaptive quizzes and assessments.", 5),
-VALUES(5, 1, "https://logicmag.io/", "Logic Magazine", "Coding challenges and competitions to sharpen skills and showcase talent, with opportunities for recognition and rewards.", 2, '../../assets/resource-thumbs/100-days.jpg'),
-VALUES(2, 1, "https://webdesign.tutsplus.com/top-15-best-practices-for-writing-super-readable-code--net-8118t", "Coding Best Practices", "Mobile app offering bite-sized coding lessons and challenges, designed for learning on the go.", 4, '../../assets/resource-thumbs/javascript.jpg'),
-VALUES(7, 1, "https://www.digitalocean.com/community/tutorials/for-loops-for-of-loops-and-for-in-loops-in-javascript", "For Loops", "Interactive coding playground for experimenting with code snippets, APIs, and libraries in a sandbox environment.", 1, '../../assets/resource-thumbs/javascript.jpg'),
+VALUES(1, 1, 'https://dev.to/devsmitra/28-javascript-array-hacks-a-cheat-sheet-for-developer-5769', 'Array Hacks Cheat Sheet', 'Personalized coding learning paths tailored to individual goals and skill levels, with adaptive quizzes and assessments.', 5),
+VALUES(5, 1, 'https://logicmag.io/', 'Logic Magazine', 'Coding challenges and competitions to sharpen skills and showcase talent, with opportunities for recognition and rewards.', 2, '../../assets/resource-thumbs/100-days.jpg'),
+VALUES(2, 1, 'https://webdesign.tutsplus.com/top-15-best-practices-for-writing-super-readable-code--net-8118t', 'Coding Best Practices', 'Mobile app offering bite-sized coding lessons and challenges, designed for learning on the go.', 4, '../../assets/resource-thumbs/javascript.jpg'),
+VALUES(7, 1, 'https://www.digitalocean.com/community/tutorials/for-loops-for-of-loops-and-for-in-loops-in-javascript', 'For Loops', 'Interactive coding playground for experimenting with code snippets, APIs, and libraries in a sandbox environment.', 1, '../../assets/resource-thumbs/javascript.jpg');
+
+-- CSS Category
+VALUES(4, 2, 'https://coolors.co/', 'Colour Palette Generator', 'Interactive coding challenges for all levels, with instant feedback and progress tracking.', 5, '../../assets/resource-thumbs/books.jpg'),
+VALUES(7, 2, 'https://unsplash.com/', 'Stock Image Resource', 'Comprehensive coding tutorials covering languages, frameworks, and tools, with practical examples and exercises.', 5, '../../assets/resource-thumbs/no-bad-days.jpg'),
+VALUES(2, 2, 'https://www.toptal.com/designers/subtlepatterns/', 'Design Patterns', 'Online coding bootcamp offering immersive, mentor-led programs in web development, data science, and more.', 3, '../../assets/resource-thumbs/comfort-zone.jpg'),
+-- should have no image
+VALUES(6, 2, 'https://cssgridgarden.com/', 'CSS Grid Garden', 'Platform providing coding courses, projects, and assessments, with a focus on real-world applications and industry relevance.', 5),
+VALUES(3, 2, 'http://www.flexboxdefense.com/', 'Flexbox Defense', 'Community-driven platform for coding enthusiasts, featuring tutorials, forums, and collaborative coding projects.', 4, '../../assets/resource-thumbs/working-hard.jpg'),
+VALUES(8, 2, 'https://github.com/awesome-css-group/awesome-css#readme', 'Awesome CSS', 'Video-based coding tutorials covering a wide range of topics, from beginner to advanced levels.', 2, '../../assets/resource-thumbs/design.jpg'),
+-- should have no image
+VALUES(1, 2, 'https://developer.mozilla.org/en-US/docs/Web/CSS/animation', 'MDN: CSS Animations', 'Personalized coding learning paths tailored to individual goals and skill levels, with adaptive quizzes and assessments.', 5),
+VALUES(5, 2, 'https://web-design-weekly.com/viewport-units-vw-vh-vmin-vmax/', 'Viewport Units', 'Coding challenges and competitions to sharpen skills and showcase talent, with opportunities for recognition and rewards.', 2, '../../assets/resource-thumbs/gitlab.jpg'),
+VALUES(2, 2, 'https://css-tricks.com/making-css-animations-feel-natural/', 'Natural CSS Animations', 'Mobile app offering bite-sized coding lessons and challenges, designed for learning on the go.', 4, '../../assets/resource-thumbs/design.jpg'),
+VALUES(7, 2, 'https://web.dev/learn/design/ui-patterns/', 'UI Patterns', 'Interactive coding playground for experimenting with code snippets, APIs, and libraries in a sandbox environment.', 1, '../../assets/resource-thumbs/books.jpg');
 
 
