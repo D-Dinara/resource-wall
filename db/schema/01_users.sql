2d203e6071591850ee36b6ca764e9232690b45d2
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2024-01-29 09:03:31 -0500

    Create tables, add seeds for categories

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,11 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "username" varchar(255) NOT NULL,
+  "first_name" varchar(255),
+  "last_name" varchar(255),
+  "email" varchar(255) NOT NULL
 );
--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -1,8 +1,37 @@
 -- Drop and recreate Widgets table (Example)
 
-DROP TABLE IF EXISTS widgets CASCADE;
-CREATE TABLE widgets (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
-  name VARCHAR(255) NOT NULL
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS resources CASCADE;
+DROP TABLE IF EXISTS likes CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
+
+CREATE TABLE "categories" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "topic" VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE "resources" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "creator_id" INTEGER REFERENCES users(id) NOT NULL,
+  "category_id" INTEGER REFERENCES categories(id),
+  "url" VARCHAR(255) NOT NULL,
+  "title" VARCHAR(255) NOT NULL,
+  "description" TEXT NOT NULL,
+  "rating" SMALLINT NOT NULL DEFAULT 0,
+  "thumbnail_url" VARCHAR(255)
+);
+
+CREATE TABLE "likes" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "owner_id" INTEGER REFERENCES users(id) NOT NULL,
+  "resource_id" INTEGER REFERENCES resources(id) NOT NULL
+);
+
+CREATE TABLE "comments" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "commentor_id" INTEGER REFERENCES users(id) NOT NULL,
+  "resource_id" INTEGER REFERENCES resources(id) NOT NULL,
+  "text" TEXT NOT NULL,
+  "date_posted" TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
