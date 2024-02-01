defd72eaceace1940b527aec26a891a642f4e405
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-02-01 11:52:12 -0500

    prepare users for AJAX request

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -1,6 +1,16 @@
-$(() => {
+$(document).ready(() => {
+  // Protect page from script injection
+  const escape = (str) => {
+    let span = document.createElement("span");
+    span.appendChild(document.createTextNode(str));
+    return span.innerHTML;
+  };
+  // display edit modal on button click
   $('#edit').on('click', function (e) {
     e.preventDefault();
     $(this).siblings('.modal').removeClass('hidden');
   });
+
+  // user AJAX request to edit DB user when user_form-edit is submitted
+
 });
