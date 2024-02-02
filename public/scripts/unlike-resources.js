45af3b033bb98b3b15c080c6794bcc36c3a00f84
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-02-02 13:13:44 -0500

    edit id and classes so delete and unlike functions work on every button instead of only the first button instance on the page

--- public/scripts/unlike-resources.js
+++ public/scripts/unlike-resources.js
@@ -1,9 +1,8 @@
 $(document).ready(() => {
   // AJAX DELETE request to remove a different user's create resource from the logged-in user's profile page. This does NOT remove the resource from the DB, only the like connection
-  $('#btn_unlike').on('click', function (e) {
+  $('.btn_unlike').on('click', function (e) {
     e.preventDefault();
-    const likesId = $(this).parent().parent().attr('id');
-    console.log("likesId", likesId);
+    const likesId = $(this).attr('id').replace("unlike_", "");
     $.ajax(`/likes/${likesId}`, { method: "DELETE" })
       .then(results => results)
       .then(() => window.location.reload())
